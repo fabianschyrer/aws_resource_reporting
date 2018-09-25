0d5aa10d0da01f644f6473487c5efef84dce570b
fabian <fabian@fabians-MacBook-Pro.local>
2018-09-25 13:10:41 +0700

    initial

---
+++ AWS_reporting_Subnets.sh
@@ -0,0 +1,35 @@
+ #!/bin/bash
+
+#########################################################################################################################
+#																														#
+#       Title:                 	AWS_reporting_Subnets.sh    	                                                    	#
+#       Purpose:                Gather AWS Subnets and write output to file		 	                            		#
+#       Author:                 Fabian Alexander Schyrer, Central Group                                               	#
+#       Poasition:              General Manager, Cloud Engineering, CTO office                                        	#
+#       Date created:           2018-09-25                                                                            	#
+#                                                                                                                     	#
+#########################################################################################################################
+
+
+# vi ~/.aws/credentials 											# specicy user credentials
+# vi ~/.aws/config													# specify IAM role
+
+PROFILE=OpsAdmin													# IAM Role (must match IAM role name, specified in ~/.aws/config)
+OUTPUT_FORMAT=json													# text, table, json
+REPORT_NAME=AWS_reporting_Subnets									# Name of the Report
+OUTPUT_DIR=~/Downloads/aws_reporting_scripts/AWS_Reports			# Output Directory
+#REGION=ap-southeast-1												# region or loop though all regions (uncomment below)
+
+
+for REGION in `aws ec2 describe-regions --output text | cut -f3`
+do
+    echo -e "\n ***** Listing Subnets in region:'$REGION'..."
+    aws ec2 describe-subnets --region $REGION --profile $PROFILE --output=$OUTPUT_FORMAT > ${REPORT_NAME}.${PROFILE}.${REGION}.${OUTPUT_FORMAT}
+done
+
+
+mkdir AWS_Reports 2> /dev/null
+cd AWS_Reports
+mkdir $(date +%Y-%m-%d)_${REPORT_NAME}
+cd ..
+mv -f ${REPORT_NAME}*.${OUTPUT_FORMAT} $OUTPUT_DIR/$(date +%Y-%m-%d)_${REPORT_NAME}
