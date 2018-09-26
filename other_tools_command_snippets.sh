351d18911969044180e2ecda7a56b53ba80765b2
fabian <fabian@fabians-MacBook-Pro.local>
2018-09-26 09:40:23 +0700

    added command snippets

---
+++ other_tools_command_snippets.sh
@@ -0,0 +1,7 @@
+other tools - command snippets
+
+pip install aws-list-all
+python -m aws_list_all --query --service ec2 --region ap-southeast-1
+
+brew tap wallix/awless; brew install awless
+awless list instances --sort uptime --local
